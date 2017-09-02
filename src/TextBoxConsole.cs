af8163181123bc3f97a6db920ebe19526ebe7c9d
Matt <xmatth13@live.com>
2017-09-02 02:05:58 -0400

    Update TextBoxConsole.cs

--- src/TextBoxConsole.cs
+++ src/TextBoxConsole.cs
@@ -4,12 +4,8 @@ using System.Windows.Forms;
 
 public class TextBoxConsole : TextWriter
 {
-    TextBox output = null; //Textbox used to show Console's output.
-
-    /// <summary>
-    /// Custom TextBox-Class used to print the Console output.
-    /// </summary>
-    /// <param name="_output">Textbox used to show Console's output.</param>
+    TextBox output = null; 
+    
     public TextBoxConsole(TextBox _output)
     {
         output = _output;
@@ -17,16 +13,11 @@ public class TextBoxConsole : TextWriter
         output.WordWrap = true;
     }
 
-    //<summary>
-    //Appends text to the textbox and to the logfile
-    //</summary>
-    //<param name="value">Input-string which is appended to the textbox.</param>
     public override void Write(char value)
     {
         base.Write(value);
         output.AppendText(value.ToString());
     }
-
 
     public override Encoding Encoding
     {
